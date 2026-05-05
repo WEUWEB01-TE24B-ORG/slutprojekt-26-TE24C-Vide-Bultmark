e5fd0546e0b14550b8ee76a8f18dc0fd88dfd848
TE24C-Vide-Bultmark <vide.bultmark@elev.ga.ntig.se>
2026-05-05 09:54:00 +0200

    brottas med jvascript

---
+++ script.js
@@ -0,0 +1,27 @@
+if (document.querySelector("p.question")) {
+    
+}
+document.querySelector("p.question.sid1").textContent = "Why are lionfish considered a harmful invasive species in the Atlantic Ocean?";
+document.querySelector("button.altA.sid1").textContent = "A) They migrate seasonally and overcrowd beaches"
+document.querySelector("button.altB.sid1").textContent = "B) They improve coral reef health by eating algae"
+document.querySelector("button.altC.sid1").textContent = "C) They only feed on plants, disrupting ocean vegetation"
+document.querySelector("button.altD.sid1").textContent = "D) They reproduce quickly and have few natural predators"
+document.querySelector("p.question.sid2").textContent = "Why are Sea bunnies?";
+document.querySelector("button.altA.sid2").textContent = "A) Sea bunny"
+document.querySelector("button.altB.sid2").textContent = "B) Sea bunny"
+document.querySelector("button.altC.sid2").textContent = "C) Sea bunny"
+document.querySelector("button.altD.sid2").textContent = "D) Sea bunny"
+document.querySelector("p.question.sid3").textContent = "Why are lionfish considered a harmful invasive species in the Atlantic Ocean?";
+document.querySelector("button.altA.sid3").textContent = "They migrate seasonally and overcrowd beaches"
+document.querySelector("button.altB.sid3").textContent = "They improve coral reef health by eating algae"
+document.querySelector("button.altC.sid3").textContent = "They only feed on plants, disrupting ocean vegetation"
+document.querySelector("button.altD.sid3").textContent = "They reproduce quickly and have few natural predators"
+document.querySelector("p.question.sid4").textContent = "Why are lionfish considered a harmful invasive species in the Atlantic Ocean?";
+document.querySelector("button.altA.sid4").textContent = "They migrate seasonally and overcrowd beaches"
+document.querySelector("button.altB.sid4").textContent = "They improve coral reef health by eating algae"
+document.querySelector("button.altC.sid4").textContent = "They only feed on plants, disrupting ocean vegetation"
+document.querySelector("button.altD.sid4").textContent = "They reproduce quickly and have few natural predators"
+
+function Correct() {
+    document.querySelector("p.question").textContent = "Correct! You are amazing!"
+}
